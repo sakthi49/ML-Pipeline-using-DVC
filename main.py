a9066adb9d3e510188e9f061061cf7265af0c11a
sakthi49 <sakthikumaran.1997@gmail.com>
2024-12-27 16:29:38 +0530

    project_setup_done

---
+++ main.py
@@ -0,0 +1,3 @@
+from src.demo import Sakthi
+obj=Sakthi()
+obj.print_details()
---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name = "MlPipeline",
+    version= "0.0.0",
+    author="Sakthi",
+    author_email="sakthikumaran.1997@gmail.com",
+    packages= find_packages(),
+    install_requires=[]
+)
