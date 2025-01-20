c77909f7e5c1aa2e3364b046f7a11f23014cf2ef
sakthi49 <sakthikumaran.1997@gmail.com>
2025-01-20 20:19:58 +0530

    change

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import find_packages, setup
 setup(
     name = "MlPipeline",
     version= "0.0.0",
-    author="Sakthi",
+    author="sakthi49",
     author_email="sakthikumaran.1997@gmail.com",
     packages= find_packages(),
     install_requires=[]
